bbd6fb725cc66cdd4a5e0f4ae3dc545603eaacbf
alex krause <alex@populi.co>
2025-11-25 10:44:44 -0800

    Setting up

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## Usage
 
 `cargo build --release`
+
 `./aoc $DAY $PART [$TEST] # $TEST is 1 or 0 or nothing. Will default to 1.`
+
 `#Also --debug to enable logging`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
+    // 1. Read in the inputs
+    // Inputs look like
+    // 5 1 0 --debug
+    // 2 2 --debug
+    // 2 1 1
+    // 4 2
+    let mut args = std::env::args();
+    let _called_exec = args.next();
+    let day : usize = args.next().expect("Missing DAY argument").parse().expect("DAY argument must be a number");
+    let part : usize = args.next().expect("Missing PART argument").parse().expect("PART argument must be a number");
+    let mut debug_enabled = false;
+    let mut use_test_set = true;
+    if let Some(third_arg) = args.next() {
+        if third_arg == "--debug" {
+            debug_enabled = true;
+        }
+        else if third_arg == "0" {
+            use_test_set = false;
+        }
+        else {
+            eprintln!("Unused third argument!");
+        }
+    }
+    if let Some(fourth_arg) = args.next() {
+        if fourth_arg == "--debug" {
+            debug_enabled = true;
+        }
+        else {
+            eprintln!("Unused fourth argument!");
+        }
+    }
+
+    // 2. Select the correct data file
+    let mut data_file_name = format!("{day:02}_{part}");
+    if use_test_set {
+        data_file_name += "_test";
+    }
+    std::fs::
+
+    // Call the correct day
     println!("Hello, world!");
 }
