c163abc03008a1719c4bde39e546e90d40daaf1e
krause-a <xander876@gmail.com>
2025-12-01 08:50:55 -0800

    Day 1

--- src/day_01.rs
+++ src/day_01.rs
@@ -2,9 +2,59 @@
 use crate::utility::*;
 
 pub fn part_1(test_data: TestData) -> String {
-    return test_data.get_string().unwrap();
+    let mut pos : isize = 50;
+    let mut lines = test_data.get_lines().unwrap();
+    let mut zeros = 0;
+    while let Some(line) = lines.next() {
+        let line = line.unwrap();
+        if line.starts_with("R") {
+            pos += line[1..].parse::<isize>().unwrap();
+        }
+        else {
+            pos -= line[1..].parse::<isize>().unwrap();
+        }
+        while pos < 0 {
+            pos += 100
+        }
+        if pos >= 100 {
+            pos %= 100
+        }
+        if pos == 0 {
+            zeros += 1
+        }
+    }
+    return zeros.to_string();
 }
 
 pub fn part_2(test_data: TestData) -> String {
-    return test_data.get_string().unwrap();
+    let mut pos : isize = 50;
+    let mut lines = test_data.get_lines().unwrap();
+    let mut zeros = 0;
+    while let Some(line) = lines.next() {
+        let line = line.unwrap();
+        let mut pos_was_zero = pos == 0;
+        if line.starts_with("R") {
+            pos += line[1..].parse::<isize>().unwrap();
+        }
+        else {
+            pos -= line[1..].parse::<isize>().unwrap();
+        }
+        while pos < 0 {
+            pos += 100;
+            zeros += 1;
+            if pos_was_zero {
+                zeros -= 1;
+                pos_was_zero = false;
+            }
+        }
+        if pos == 0 {
+            zeros += 1
+        }
+        while pos >= 100 {
+            pos -= 100;
+            zeros += 1;
+        }
+        log::debug(&format!("{:04}: {pos} {}", zeros, line));
+    }
+    return zeros.to_string();
 }
