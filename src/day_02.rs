65879b4c284a28d496784dc9a841d0abe06f5b66
krause-a <xander876@gmail.com>
2025-12-02 07:18:32 -0800

    Not working part 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -43,5 +43,52 @@ pub fn part_1(test_data: TestData) -> String {
 }
 
 pub fn part_2(test_data: TestData) -> String {
-    return test_data.get_string().unwrap();
+    let mut invalid_ids : Vec<(String, u8)> = Vec::new();
+
+    let mut words = test_data.get_words(",").unwrap();
+
+    while let Some(range) = words.next() {
+        let (low, high) = range.trim().split_once("-").unwrap();
+        log::debug(|| format!("Word: {low}-{high}"));
+
+        if low.len() == high.len() && low.len() & 1 != 0 {
+            continue; // Don't check odd length IDs
+        }
+
+        let mut id = low.to_string();
+        while str_num_compare(&id, high) != cmp::Ordering::Greater {
+            // if id.len() & 1 != 0 {
+            //     id = "1".to_string() + &"0".repeat(id.len());
+            // }
+            let half_len = id.len() / 2;
+            let half_id = &id[..half_len];
+            for repeats in 2..=id.len() {
+                if id.len() % repeats != 0 {
+                    continue;
+                }
+                let frac = id.len() / repeats;
+                let id_frac = id[..frac].to_string();
+                log::debug(|| format!("Repeats: {repeats}, Frac: {id_frac}, ID: {id}"));
+                let mirror_id = id_frac.repeat(repeats);
+                if str_num_compare(&mirror_id, high) != cmp::Ordering::Greater && str_num_compare(&mirror_id, low) != cmp::Ordering::Less {
+                    invalid_ids.push((id_frac, repeats as u8));
+                    log::debug(|| format!("Low: {low}, High: {high}, Current: {mirror_id}"));
+                    log::info(|| format!("Invalid ID Found {mirror_id}"));
+                    if str_num_compare(&mirror_id, &id) == cmp::Ordering::Greater {
+                        id = mirror_id;
+                    }
+                }
+            }
+            id = (id.parse::<usize>().unwrap() + 1).to_string();
+        }
+    }
+
+    let mut sum : usize = 0;
+    for (invalid, repeats) in invalid_ids {
+        let rep_id = invalid.repeat(repeats as usize);
+        let full_id = rep_id.parse::<usize>().unwrap();
+        sum += full_id;
+    }
+    
+    return sum.to_string();
 }
