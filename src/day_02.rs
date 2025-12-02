7dc463a5207dc53a9856ee4d459ab13f89518c51
krause-a <xander876@gmail.com>
2025-12-01 22:13:11 -0800

    Day 2 part 1

--- src/day_02.rs
+++ src/day_02.rs
@@ -1,8 +1,45 @@
 #![allow(unused, dead_code)]
 use crate::utility::*;
+use std::cmp;
 
 pub fn part_1(test_data: TestData) -> String {
-    return test_data.get_string().unwrap();
+    let mut invalid_ids : Vec<String> = Vec::new();
+
+    let mut words = test_data.get_words(",").unwrap();
+
+    while let Some(range) = words.next() {
+        let (low, high) = range.trim().split_once("-").unwrap();
+        log::debug(|| format!("Word: {low}-{high}"));
+
+        if low.len() == high.len() && low.len() & 1 != 0 {
+            continue; // Don't check odd length IDs
+        }
+
+        let mut id = low.to_string();
+        while str_num_compare(&id, high) != cmp::Ordering::Greater {
+            if id.len() & 1 != 0 {
+                id = "1".to_string() + &"0".repeat(id.len());
+            }
+            let half_len = id.len() / 2;
+            let half_id = &id[..half_len];
+            let mirror_id = half_id.to_string() + half_id;
+            if str_num_compare(&mirror_id, high) != cmp::Ordering::Greater && str_num_compare(&mirror_id, low) != cmp::Ordering::Less {
+                invalid_ids.push(half_id.to_string());
+                log::debug(|| format!("Low: {low}, High: {high}, Current: {mirror_id}"));
+                log::info(|| format!("Invalid ID Found {mirror_id}"));
+            }
+            let half_id_num = half_id.parse::<usize>().unwrap() + 1;
+            id = half_id_num.to_string().repeat(2);
+        }
+    }
+
+    let mut sum = 0;
+    for invalid in invalid_ids {
+        let full_id = (invalid.clone() + &invalid).parse::<usize>().unwrap();
+        sum += full_id;
+    }
+    
+    return sum.to_string();
 }
 
 pub fn part_2(test_data: TestData) -> String {
--- src/utility.rs
+++ src/utility.rs
@@ -1,6 +1,6 @@
 #![allow(unused, dead_code)]
 
-use std::{fs, io::{self, BufRead, Bytes, Lines, Read}};
+use std::{cmp, fs, io::{self, BufRead, Bytes, Lines, Read}};
 
 pub struct TestData {
     file_path: std::path::PathBuf,
@@ -29,6 +29,40 @@ impl TestData {
         let reader = io::BufReader::new(f);
         Ok(reader.assume_chars())
     }
+    pub fn get_words(self: &Self, separator: &'static str) -> Result<AssumeWords<io::BufReader<fs::File>>, io::Error> {
+        let f = self.open_file()?;
+        let reader = io::BufReader::new(f);
+        Ok(reader.assume_words(separator))
+    }
+}
+
+pub struct AssumeWords<R: BufRead> {
+    inner: AssumeChars<R>,
+    sep: &'static str,
+    word: String,
+}
+
+impl<R: BufRead> Iterator for AssumeWords<R> {
+    type Item = String;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            while !self.word.ends_with(self.sep) && let Some(c) = self.inner.next() {
+                self.word.push(c);
+            }
+            if self.word.ends_with(self.sep) {
+                self.word = self.word.trim_end_matches(self.sep).to_string();
+                if self.word.len() == 0 {
+                    continue;
+                }
+            }
+            if self.word.len() == 0 {
+                return None;
+            }
+            else {
+                return Some(std::mem::take(&mut self.word));
+            }
+        }
+    }
 }
 
 pub struct AssumeLines<R: BufRead> {
@@ -48,6 +82,9 @@ trait AsssumeBufRead: BufRead + Sized {
     }
     fn assume_chars(self) -> AssumeChars<Self> {
         AssumeChars { inner: self.bytes() }
+    }
+    fn assume_words(self, sep: &'static str) -> AssumeWords<Self> {
+        AssumeWords { inner: self.assume_chars(), sep: sep, word: String::new() }
     }
 }
 
@@ -136,4 +173,28 @@ pub mod log {
             write!(f, "LogLevel parse error: {}", self.0)
         }
     }
+}
+
+pub fn str_num_compare(a: &str, b: &str) -> cmp::Ordering {
+    if a.len() != b.len() {
+        if a.len() > b.len() {
+            return cmp::Ordering::Greater;
+        }
+        else {
+            return cmp::Ordering::Less;
+        }
+    }
+    else {
+        for i in 0..a.len() {
+            let a_digit = a[i..=i].parse::<u8>().unwrap();
+            let b_digit = b[i..=i].parse::<u8>().unwrap();
+            if a_digit > b_digit {
+                return cmp::Ordering::Greater;
+            }
+            else if a_digit < b_digit {
+                return cmp::Ordering::Less;
+            }
+        }
+    }
+    return cmp::Ordering::Equal;
 }
