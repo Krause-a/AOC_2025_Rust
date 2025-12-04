0640a11113a70128bdc3ab2417983a2bf6c5cd26
krause-a <xander876@gmail.com>
2025-12-03 22:12:07 -0800

    Add day 4 incomplete

--- src/day_04.rs
+++ src/day_04.rs
@@ -2,7 +2,23 @@
 use crate::utility::*;
 
 pub fn part_1(test_data: TestData) -> String {
-    return String::from("Wowies Day 4 Part 1");
+    let grid = test_data.get_grid().unwrap();
+    log::grid(&grid);
+    let mut forkable_paper_rolls = 0;
+    for (p, &c) in grid.iter() {
+        if c != '@' {
+            continue;
+        }
+
+        let neighboring_paper_rolls = p.all_neighbors_in(&grid).iter()
+            .filter(|&np| grid.get(np).map(|&gc| gc == '@').unwrap_or(false))
+            .count();
+        if neighboring_paper_rolls < 4 {
+            forkable_paper_rolls += 1;
+        }
+    }
+
+    return forkable_paper_rolls.to_string();
 }
 
 pub fn part_2(test_data: TestData) -> String {
